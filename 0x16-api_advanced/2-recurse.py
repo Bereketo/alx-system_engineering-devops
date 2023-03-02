cb59aa9273d99def6d5ad8e955669de8e825540f
Berekto <bereketalebachew87@gmail.com>
2023-03-02 21:31:53 +0300

    api_advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""queires the reddit api 
+"""queires the reddit api
    and returns a list containing the titles of all hot articles
 """
 
@@ -8,21 +8,30 @@ import requests
 
 def recurse(subreddit, hot_list=[], after=None):
     """recursive function that queires reddit api """
-    url = "https://api.reddit.com/r/{}?sort=hot".format(subreddit)
-    headers = {'User-Agent': 'CustomClient/1.0'}
-    params = {"limit": 100, "after": after} if after else {"limit": 100}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    
-    if response.status_code == 200:
-        data = response.json()
-        posts = data["data"]["children"]
-        after = data["data"]["after"]
-        for post in posts:
-            hot_list.append(post["data"]["title"])
-        if after:
-            recurse(subreddit, hot_list, after)
-        else:
-            return hot_list
-    else:
-        return None
 
+    if type(subreddit) is list:
+        url = "https://api.reddit.com/r/{}?sort=hot".format(subreddit[0])
+        url = "{}&after={}".format(url, subreddit[1])
+    else:
+        url = "https://api.reddit.com/r/{}?sort=hot".format(subreddit)
+        subreddit = [subreddit, ""]
+    headers = {'User-Agent': 'CustomClient/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return (None)
+    response = response.json()
+    if "data" in response:
+        data = response.get("data")
+        if not data.get("children"):
+            return (hot_list)
+        for post in data.get("children"):
+            hot_list += [post.get("data").get("title")]
+        if not data.get("after"):
+            return (hot_list)
+        subreddit[1] = data.get("after")
+        recurse(subreddit, hot_list)
+        if hot_list[-1] is None:
+            del hot_list[-1]
+        return (hot_list)
+    else:
+        return (None)
