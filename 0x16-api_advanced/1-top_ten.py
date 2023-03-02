2952526c15287f9c915143c3284bb2f2e661c0ae
Berekto <bereketalebachew87@gmail.com>
2023-03-02 18:02:27 +0300

    api_advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ import requests
 def top_ten(subreddit):
     """ a functon that queres the reddit """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {'User-Agent': 'CustomClient/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
