d30943f5584d2d20573c86f6c0d56248cd47e376
Berekto <bereketalebachew87@gmail.com>
2023-03-02 16:00:48 +0300

    api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """  queries the Reddit API and returns the number of subscribers
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
     """returns total number of subscribers """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {'User-Agent': 'CustomClient/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
